cbc9fa3ec724362f05ba4ddffaf71db91c59d022
kws <89932020+katie-ws@users.noreply.github.com>
2021-11-25 18:47:49 +0000

    Updated styling

--- src/Components/Subreddits/Subreddits.js
+++ src/Components/Subreddits/Subreddits.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Subreddits.css';
 
 export const Subreddits = () => {
+
     return (
         <div className="Subreddits">
-            <h2>Subreddits</h2>
+            <h2>Subreddits</h2>  
         </div>
     )
 };
