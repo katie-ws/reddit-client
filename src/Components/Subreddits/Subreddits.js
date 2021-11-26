1c7c0d5d8c3257b65b3c0ce00cb5bb89fb8dbafd
kws <89932020+katie-ws@users.noreply.github.com>
2021-11-26 14:10:05 +0000

    Added Subreddit component

--- src/Components/Subreddits/Subreddits.js
+++ src/Components/Subreddits/Subreddits.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Subreddits.css';
+import {Subreddit} from '../Subreddit/Subreddit';
 
 export const Subreddits = () => {
 
     return (
         <div className="Subreddits">
             <h2>Subreddits</h2>  
+            <Subreddit />
         </div>
     )
 };
